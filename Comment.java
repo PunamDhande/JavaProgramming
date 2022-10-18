db7cd377befa1436464bc27e015393769d3540aa
PunamDhande <116068802+PunamDhande@users.noreply.github.com>
2022-10-18 16:04:47 +0530

    Add files via upload

---
+++ Comment.java
@@ -0,0 +1,17 @@
+package basic_to_advance;
+/** This is java Docuent
+ */
+public class Comment {
+
+	public static void main(String[] args) {
+		//This is my first java program
+		System.out.println("I completed my graduation in Bachelor Of Computer Science");
+		/*
+		 * Your name.
+		 * Your Address. 
+		 */
+System.out.println("My name is Punam");
+System.out.println("Pune,Maharashta");
+	}
+
+}
