25956ca3c16b051ba9e46656ec0fa4cdac9bfd11
PunamDhande <116068802+PunamDhande@users.noreply.github.com>
2022-10-19 16:07:54 +0530

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,56 @@
+package com.world;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		System.out.println("Hello_World");
+		//boolean example
+boolean IoT=true;
+System.out.println("IoT");
+
+
+//byte example
+byte range;
+range=127;
+System.out.println(range);
+
+
+//short example
+short range1;
+range=-127;
+System.out.println(range);
+
+
+//int example
+int range2=3000000;
+System.out.println(range2);
+
+
+//long example
+long range3=300000000000L;
+System.out.println(range3);
+
+
+//double example
+double number=99.9;
+System.out.println(number);
+
+
+//float example
+float number1=99.9F;
+System.out.println(number1);
+
+
+//char example(Unicode value of A is'\u0041')
+char letter='\u0041';
+System.out.println(letter);
+
+
+char letter2=65; //Ascii value of A is 65
+System.out.println(letter2);
+char letter3='A';
+System.out.println(letter3);
+
+	}
+
+}
