40dc87b1413f834af7883c4925863a5f9cd9b004
PunamDhande <116068802+PunamDhande@users.noreply.github.com>
2022-10-19 10:07:17 +0530

    Add files via upload

---
+++ AreaCircle.java
@@ -0,0 +1,12 @@
+package com.Area;
+
+public class AreaCircle {
+
+	public static void main(String[] args) {
+		int radius=8;
+		double pi = 3.142 , area;
+		area = pi * radius * radius ;
+		System.out.println(" The Area of Circle with radius=8 is "+area );
+	}
+
+}
