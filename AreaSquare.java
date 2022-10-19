d5aca5b09a841a939bbe8ff5dbb25966fadac42e
PunamDhande <116068802+PunamDhande@users.noreply.github.com>
2022-10-19 09:49:30 +0530

    Add files via upload

---
+++ AreaSquare.java
@@ -0,0 +1,13 @@
+package com.Area;
+
+public class AreaSquare {
+
+	public static void main(String[] args) {
+			float side,area;
+			side = 15;
+			area = side * side;
+			System.out.println(" The Area of Square with side = 25 is "+area +"sq unit.");
+
+	}
+
+}
