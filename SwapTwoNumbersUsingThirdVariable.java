806d430c4fbee412f752012844f1bd6d60647f77
PunamDhande <116068802+PunamDhande@users.noreply.github.com>
2022-10-19 14:49:15 +0530

    Add files via upload

---
+++ SwapTwoNumbersUsingThirdVariable.java
@@ -0,0 +1,17 @@
+package basic_to_advance;
+
+public class SwapTwoNumbersUsingThirdVariable {
+
+	public static void main(String[] args) {
+			int a,b,temp;
+			a=10;
+			b=20;
+			System.out.println("Before swapping a="+a+ "and b="+b);
+			temp=a;
+			a=b;
+			b=temp;
+			System.out.println("After swapping a="+a+ "and b="+b);
+
+	}
+
+}
