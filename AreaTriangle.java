dea44993a8608f866a8e87b9220b26f80aafafa0
PunamDhande <116068802+PunamDhande@users.noreply.github.com>
2022-10-19 09:56:16 +0530

    Add files via upload

---
+++ AreaTriangle.java
@@ -0,0 +1,14 @@
+package com.Area;
+
+public class AreaTriangle {
+
+	public static void main(String[] args) {
+			float base,height,area;
+			base = 8;
+			height=11;
+			area =(base * height)/2;
+			System.out.println(" The Area of Triangle of base= 8 and height=11 is "+area +"sq unit.");
+
+	}
+
+}
