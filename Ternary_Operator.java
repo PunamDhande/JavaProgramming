8d059deeb4b31a950e57ac6401607a65d79045eb
PunamDhande <116068802+PunamDhande@users.noreply.github.com>
2022-10-19 16:27:05 +0530

    Add files via upload

---
+++ Ternary_Operator.java
@@ -0,0 +1,13 @@
+package com.world;
+
+public class Ternary_Operator {
+
+	public static void main(String[] args) {
+int a,b,large;
+a=15;
+b=22;
+large=(a>b)?a:b;
+System.out.println(large);
+	}
+
+}
