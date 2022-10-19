4b266f13709f20d715cd279caf49879b1b2d5e46
PunamDhande <116068802+PunamDhande@users.noreply.github.com>
2022-10-19 09:45:08 +0530

    Add files via upload

---
+++ AreaReactangle.java
@@ -0,0 +1,14 @@
+package com.Area;
+
+public class AreaReactangle {
+
+	public static void main(String[] args) {
+		int length,breadth,area;
+		length = 15;
+		breadth=11;
+		area = length * breadth;
+		System.out.println(" The Area of Rectangle of length=15 and breadth=11 is "+area +"sq unit.");
+
+	}
+
+}
