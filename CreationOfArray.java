38490600c8894a38b32cfe88ac5b632b971b2495
PunamDhande <116068802+PunamDhande@users.noreply.github.com>
2022-10-30 22:55:30 +0530

    Add files via upload

---
+++ CreationOfArray.java
@@ -0,0 +1,21 @@
+package com.edu;
+
+public class CreationOfArray {
+
+	public static void main(String[] args) {
+char[] ch=new char[5];
+ch[0]='P';
+ch[1]='U';
+ch[2]='N';
+ch[3]='A';
+ch[4]='M';
+
+System.out.println(ch[0]);
+System.out.println(ch[1]);
+System.out.println(ch[2]);
+System.out.println(ch[3]);
+System.out.println(ch[4]);
+System.out.println(ch[5]);
+		}
+
+}
