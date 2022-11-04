a2c5102c43a09c65d16f1b1111031f5dfc274c6a
PunamDhande <116068802+PunamDhande@users.noreply.github.com>
2022-11-04 10:02:25 +0530

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,31 +1,26 @@
-package com.edu;
+package p5;
 
-class ParentClass{
-	int i,j;
-	ParentClass(int i,int j)//uses same variable like instance variable so we use this class
-	{
-		System.out.println("Parent class constructor");
-		this.i=i;
-		this.j=j;
-	}
-}
-class ChildClass extends ParentClass {//single Inheritance
+import p6.ParentClass;
+
+class ChildClass extends ParentClass{//single iheritance
 int s;
-
 ChildClass(int i,int j){
 	super(i,j);
-	System.out.println("Child class constructor");
+	System.out.println("Child Constructor");
 }
 void add() {
 	s=i+j;
 	System.out.println("sum="+s);
 }
+}
 
-}
 public class MainClass {
-public static void main(String args[]) {
-	
-	ChildClass ob=new ChildClass(8,9);
-	ob.add();
-}
+
+	public static void main(String[] args) {
+ChildClass ob=new ChildClass(8,9);
+ob.add();
+ParentClass pob=new ParentClass(9,8);
+System.out.println("Protected data="+pob.k);
+	}
+
 }
