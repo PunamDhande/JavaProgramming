442c82139ab8ca0b1a889dc4f3f441f888e52f2d
PunamDhande <116068802+PunamDhande@users.noreply.github.com>
2022-11-10 07:08:56 +0530

    Add files via upload

---
+++ PatternNumSpace.java
@@ -0,0 +1,21 @@
+package com.patterns;
+
+public class PatternNumSpace {
+
+	public static void main(String[] args) {
+		
+				int k=1;
+				for(int i=1;i<=4;i++)
+				{
+					for(int b=4-i;b>1;b--)
+					{
+						System.out.print("");
+					}
+					for(int j=1;j<=i+1;j++)
+					{
+						System.out.print(k++ +" ");
+					}
+					System.out.println();
+				}
+	}
+}
--- PatternReverse.java
+++ PatternReverse.java
@@ -2,7 +2,7 @@ package com.patterns;
 
 import java.util.Scanner;
 
-public class Pattern1 {
+public class PatternReverse {
 
 	public static void main(String[] args) {
 int r;
@@ -10,8 +10,8 @@ Scanner sc=new Scanner(System.in);
 System.out.println("Enter the number Of row:");
 r=sc.nextInt();
 
-for(int i=1;i<=r;i++) {
-	for(int b=r-i;b>1;b--) {
+for(int i=r;i>=1;i--) {
+	for(int b=1;b<=r-i;b++) {
 		System.out.print(" ");
 	}
 	for(int j=1;j<=i;j++) {
