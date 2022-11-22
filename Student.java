cd066452d71e7f4447e55bca6143e069bd78a809
PunamDhande <116068802+PunamDhande@users.noreply.github.com>
2022-11-22 23:26:11 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,7 +1,35 @@
-package p3;
+package MemoryManagement;
 
 public class Student {
-public void gradeStudent() {
-	System.out.println("Grade");
+
+int a;
+int b;
+
+public void setData(int c,int d) {
+	a=c;
+	b=d;
 }
+public void showData() {
+	System.out.println("Value of a="+a);
+	System.out.println("Value of b="+b);
+}
+
+	public static void main(String[] args) {
+		Student s1=new Student();
+		Student s2=new Student();
+		s1.setData(1, 2);
+		s2.setData(4, 6);
+		s1.showData();
+		
+		Student s3;
+		s3=s2;
+		s3.showData();
+		
+		s2=null;
+		s2.showData();
+		s3.showData();
+		//s3=null
+		
+	}
+
 }
